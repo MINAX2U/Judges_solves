9fac86e39293c8b3ee94b7d6fc9f3516ff56bf45
賴奕辰 <ethanlai588@proton.me>
2025-07-26 13:57:57 +0800

    BFS

--- Zero_judge/原創不分類題庫/d453.cpp
+++ Zero_judge/原創不分類題庫/d453.cpp
@@ -1,39 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-map <char, vector<pair<char, int>>> mp;
-char start;
-
-int f(int start, int total){
-  if(!mp.count(start))return total;
-
-  int maxi = 0;
-
-  for(pair<char, int> p:mp[start]){
-    maxi = max(f(p.first, total+p.second), maxi);
-  }
-
-  return maxi;
-}
-
-
-
+const int dx[4] = {0, 0, 1, -1};
+const int dy[4] = {1, -1, 0, 0};
 
 int main(){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int num;
-  while(cin>>start>>num){
-    mp.clear();
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-    char a, b;
-    int n;
+  int T;
+  cin>>T;
+  while(T--){
+    int n, m, sx, sy, ex, ey;
+    cin>>n>>m>>sx>>sy>>ex>>ey;
 
-    for(int i=0;i<num;i++){
-      cin>>a>>b>>n;
-      mp[a].push_back({b, n});
+    sx--,sy--,ex--,ey--;
+
+    vector<string> grid(n);
+    for(int i=0;i<n;++i){
+      cin>>grid[i];
     }
 
-    cout << f(start, 0) <<"\n";
+    vector<vector<int>> dist(n, vector<int>(m, -1));
+    queue<pair<int, int>> q;
+    q.push({sx, sy});
+    dist[sx][sy] = 0;
+
+    while(!q.empty()){
+      auto [x,y] = q.front(); q.pop();
+
+      for(int i=0; i<4; ++i){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if(nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == '0' && dist[nx][ny] == -1){
+          dist[nx][ny] = dist[x][y] + 1;
+          q.push({nx, ny});
+        }
+      }
+    }
+    if(dist[ex][ey] == -1) cout << "0\n";
+    else cout<<dist[ex][ey]+1<<"\n";//因為包含起點所以+1
   }
 }
